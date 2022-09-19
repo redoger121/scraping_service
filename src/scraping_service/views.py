c2570f926e562a5c4116624f90da8735ae45f71a
redoger121 <english121ru123>
2022-09-19 14:42:17 +0700

    first commit

---
+++ src/scraping_service/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
+
+
+def home(request):
+    date=datetime.datetime.now().date()
+    name='Dave'
+    context={'date':date, 'name':name}
+    return render(request, 'home.html', context)
